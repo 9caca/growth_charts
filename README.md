20ae2be494d9296fbee161b31e44c36f5ac77df5
Carlos Hayashi <carlos.hiroshi.hayashi@gmail.com>
2025-01-25 02:23:19 -0400

    inclusao do grafico da estatura e menu lateral para novos dados

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 CDC Growth Charts Data Files
 https://www.cdc.gov/growthcharts/cdc-data-files.htm
+
+
+<h1>Dashboard acompanhamento de crescimento criancas</h1>
+
+##  💡 Descrição
+Este é um projeto para criar programa para acompanhamento de controle de crescimento e peso de criancas.
+O objetivo é praticar programação Python e explorar conceitos de Data Science e apresentacao de dados.  
+
+
+##  💡 Pré-requisitos
+
+* Python 3.9+ ![Python](https://img.shields.io/badge/Code-Python-informational?style=flat&logo=python&color=3776AB)
+* Dependências do projeto:
+  * `pandas`
+  * `streamlit`
+  * `plotly_express`
+---
+
+##  💡 Origem dos dados
+
+CDC Growth Charts Data Files
+https://www.cdc.gov/growthcharts/cdc-data-files.htm
+
+___
+  
+##  💡 Meta
+- Carlos Hayashi - @9caca
+- Distribuído sob a licença [MIT License](https://https://github.com/9caca/AirCnC/blob/master/LICENSE)
--- main.py
+++ main.py
@@ -11,6 +11,8 @@ mariana_birthdate = datetime(2018, 3, 18)
 #Definir a largura do dashboard para largura total da tela
 st.set_page_config(layout="wide")
 
+altura_df = pd.read_csv("stature.csv")
+altura_df = altura_df[altura_df['Sex'] == 2]
 bmi_df = pd.read_csv("bmi.csv")
 weight_df = pd.read_csv("weight.csv")
 weight_df = weight_df[weight_df['Sex'] == 2]
@@ -21,7 +23,6 @@ st.sidebar.title("Opções")
 selected_child = st.sidebar.selectbox("Selecione a criança:", ["Mariana", "Nina"])
 file_name = "mariana_peso.csv" if selected_child == "Mariana" else "nina_peso.csv"
 birthdate = mariana_birthdate if selected_child == "Mariana" else nina_birthdate
-
 
 # Transformar a idade de meses para anos e meses
 def agemos_to_years_months(agemos):
@@ -54,24 +55,26 @@ else:
 st.sidebar.subheader(f"Adicionar peso para {selected_child}")
 input_date = st.sidebar.date_input("Data da pesagem:")
 input_weight = st.sidebar.number_input("Peso (kg):", min_value=0.0, step=0.1)
+input_stature = st.sidebar.number_input("Altura (cm):", min_value=0.0, step=0.1)
 
 if st.sidebar.button("Salvar"):
-    # Calcular idade em anos
+    # Calcular idade em meses
     try:
-        # Converter input_date para datetime.datetime
-        input_date_datetime = datetime.combine(input_date, datetime.min.time())
-
-        # Calcular idade em anos
-        age_years = (input_date_datetime - birthdate).days / 365.25
-
+        # Garantir que input_date e birthdate estejam no mesmo formato
+        input_date = datetime.combine(input_date, datetime.min.time())  # Converte input_date para datetime.datetime
+        birthdate = birthdate.replace(hour=0, minute=0, second=0, microsecond=0)  # Garante que birthdate também está no formato datetime.datetime
+        
+        # Calcular idade em meses
+        age_months = round((input_date - birthdate).days / 30.4375)
+        
         # Adicionar entrada ao arquivo CSV
-        new_data = pd.DataFrame({"Date": [input_date], "Age (years)": [age_years], "Weight": [input_weight]})
+        new_data = pd.DataFrame({"Date": [input_date], "agemos": [age_months], "Weight": [input_weight], "stature": [input_stature]})
         try:
             existing_data = pd.read_csv(file_name)
             updated_data = pd.concat([existing_data, new_data])
         except FileNotFoundError:
             updated_data = new_data
-
+        
         updated_data.to_csv(file_name, index=False)
         st.sidebar.success(f"Dados salvos para {selected_child}!")
     except Exception as e:
@@ -80,15 +83,18 @@ if st.sidebar.button("Salvar"):
 # Carregar os dados de Nina ou Mariana
 try:
     child_data = pd.read_csv(file_name)
-except FileNotFoundError:
-    child_data = pd.DataFrame(columns=["Date", "Age (years)", "Weight"])
+    if child_data.empty:
+        child_data = pd.DataFrame(columns=["Date", "agemos", "Weight", "stature"])
+except (FileNotFoundError, pd.errors.EmptyDataError):
+    child_data = pd.DataFrame(columns=["Date", "agemos", "Weight", "stature"])
 
+child_data['Age (formatted)'] = child_data['agemos'].apply(agemos_to_years_months)
 # Criar o gráfico
-fig = go.Figure()
+fig_weight = go.Figure()
 
 # Adicionar curvas de crescimento
 for percentile in ["P5", "P10", "P25", "P50", "P75", "P90", "P95"]:
-    fig.add_trace(
+    fig_weight.add_trace(
         go.Scatter(
             x=weight_df['Age (years)'],
             y=weight_df[percentile],
@@ -100,9 +106,10 @@ for percentile in ["P5", "P10", "P25", "P50", "P75", "P90", "P95"]:
 
 # Adicionar os dados de Nina ou Mariana
 if not child_data.empty:
-    fig.add_trace(
+    child_data['Age (years)'] = child_data['agemos'].apply(lambda x: x / 12)  # Transformar meses em anos (número decimal)
+    fig_weight.add_trace(
         go.Scatter(
-            x=child_data['Age (years)'],
+            x=child_data['Age (formatted)'],
             y=child_data['Weight'],
             mode='markers+lines',
             name=f"{selected_child} (dados)",
@@ -112,14 +119,66 @@ if not child_data.empty:
     )
 
 # Configurar layout do gráfico
-fig.update_layout(
+fig_weight.update_layout(
     title='Curvas de Crescimento - Meninas',
     xaxis_title='Idade (anos)',
     yaxis_title='Peso (kg)',
     height=700,
-    yaxis=dict(range=[10, 50]),
+    yaxis=dict(range=[13, 45]),
     xaxis=dict(range=[24, 96])
 )
 
 # Exibir o gráfico
-st.plotly_chart(fig)
+st.plotly_chart(fig_weight)
+
+altura_df['Age (years)'] = altura_df['Agemos'].apply(agemos_to_years_months)
+child_data['Age (formatted)'] = child_data['agemos'].apply(agemos_to_years_months)
+
+# Criar o gráfico
+fig_stature = go.Figure()
+# Criar o gráfico de altura
+if not altura_df.empty:
+    fig_stature = go.Figure()
+    for col in ['P5', 'P10', 'P25', 'P50', 'P75', 'P90', 'P95']:
+        fig_stature.add_trace(
+            go.Scatter(
+                x=altura_df['Age (years)'],  # Converter idade em meses para anos
+                y=altura_df[col],
+                mode='lines',
+                name=f"{col} - Altura",
+                line=dict(
+                    color='black' if col in ['P5', 'P95'] else (
+                        'orange' if col in ['P10', 'P90'] else (
+                        'green' if col in ['P25', 'P75'] else 'blue'
+                        )
+                    ),
+                    dash='solid'
+                )
+            )
+        )
+    # Adicionar os dados de Nina ou Mariana
+if not child_data.empty:
+    child_data['Age (years)'] = child_data['agemos'].apply(lambda x: x / 12)  # Transformar meses em anos (número decimal)
+    fig_stature.add_trace(
+        go.Scatter(
+            x=child_data['Age (formatted)'],
+            y=child_data['stature'],
+            mode='markers+lines',
+            name=f"{selected_child} (dados)",
+            line=dict(color='pink', dash='dot'),
+            marker=dict(size=10)
+        )
+    )
+
+    # Adicionar título e ajustar layout
+    fig_stature.update_layout(
+        title="Curvas de Crescimento - Altura (Meninas)",
+        xaxis_title="Idade (anos)",
+        yaxis_title="Altura (cm)",
+        height=700,
+        xaxis=dict(tickmode='linear', dtick=1, range=[24, 96]),
+        yaxis=dict(range=[95, 145]),  # Ajustar o limite máximo para altura
+        legend_title="Percentis"
+        
+    )
+    st.plotly_chart(fig_stature)
